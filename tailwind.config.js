88989d15e24bf1bbf408b8d6dea6bb60fad7e9c3
GelfisHorn <helphis.tech@gmail.com>
2023-05-13 21:09:13 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				blacks: {
+					eerie: '#17171B',
+					raisin: '#1A1C23',
+				},
+				whites: {
+					platinum: '#DEE5E5',
+				},
+				reds: {
+					crayola: '#ED254E',
+				},
+			},
+		},
+	},
+	plugins: [],
 }
